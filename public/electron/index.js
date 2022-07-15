fbe44e50387a34b8068fb4da463d40df6711ed08
Rafael Willen <rafaelpadre20@gmail.com>
2022-07-15 16:37:28 +0100

    setup electron app

---
+++ public/electron/index.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+
+const DEV_URL = "http://localhost:3000"
+const PROD_URL = `file://${path.join(__dirname, "../../index.html")}`
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        webPreferences: {
+            nodeIntegration: true,
+        }
+    });
+
+    win.loadURL(isDev ? DEV_URL : PROD_URL);
+
+    if (isDev)
+        win.webContents.openDevTools({ mode: "detach" });
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+});
+
+app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length == 0) {
+        createWindow();
+    }
+})
